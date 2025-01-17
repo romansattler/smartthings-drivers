ecf1dcf6ed731505e21c40d67beb9af2af80ff90
Roman Sattler <roman.sattler@gmail.com>
2025-01-17 02:44:20 +0100

    fixed zigfred endpoint map

--- drivers/zigfred/src/init.lua
+++ drivers/zigfred/src/init.lua
@@ -7,6 +7,18 @@ local clusters = require "st.zigbee.zcl.clusters"
 
 local Level = clusters.Level
 local OnOff = clusters.OnOff
+local ColorControl = clusters.ColorControl
+
+local function supports_cluster(ep, cluster_id)
+  local supports_cluster = false
+  for _, cluster in ipairs(ep.server_clusters) do
+    if cluster == cluster_id then
+      supports_cluster = true
+    end
+  end
+
+  return supports_cluster
+end
 
 local function find_child(parent, ep_id)
   return parent:get_child_by_parent_assigned_key(string.format("%02X", ep_id))
@@ -23,34 +35,31 @@ end
 local function added_handler(driver, device)
   log.debug("added_handler", driver, device)
 
-  if device.network_type == st_device.NETWORK_TYPE_ZIGBEE then
-    for ep_id, ep in pairs(device.zigbee_endpoints) do
-      if ep_id ~= device.fingerprinted_endpoint_id then
-        if find_child(device, ep_id) == nil then
-          local profile = nil
-          for _, cluster in ipairs(ep.server_clusters) do
-            if (profile == nil and cluster == OnOff.ID) then
-              profile = "relay"
-            end
+  if device.network_type ~= st_device.NETWORK_TYPE_ZIGBEE then
+    return
+  end
 
-            if (cluster == Level.ID) then
-              profile = "dimmer"
-            end
-          end
+  for ep_id, ep in pairs(device.zigbee_endpoints) do
+    if find_child(device, ep_id) == nil then
+      local switchable = supports_cluster(ep, OnOff.ID)
+      local dimmable = supports_cluster(ep, Level.ID)
+      local color = supports_cluster(ep, ColorControl.ID)
 
-          if profile ~= nil then
-            local name = device.label .. " " .. profile
-            local metadata = {
-              type = "EDGE_CHILD",
-              label = name,
-              profile = "zigfred-" .. profile,
-              parent_device_id = device.id,
-              parent_assigned_child_key = string.format("%02X", ep_id),
-              vendor_provided_label = name
-            }
-            driver:try_create_device(metadata)
-          end
-        end
+      local profile = color and switchable and dimmable and "indicator" or
+          switchable and dimmable and "dimmer" or
+          switchable and "relay" or nil
+
+      if profile ~= nil then
+        local name = device.label .. " " .. profile
+        local metadata = {
+          type = "EDGE_CHILD",
+          label = name,
+          profile = "zigfred-" .. profile,
+          parent_device_id = device.id,
+          parent_assigned_child_key = string.format("%02X", ep_id),
+          vendor_provided_label = name
+        }
+        driver:try_create_device(metadata)
       end
     end
   end
